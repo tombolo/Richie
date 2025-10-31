3cbac21e6f6f7dded8a0744543436d2061d69bc6
Joseph Mumo <72470955+tombolo@users.noreply.github.com>
2025-10-31 12:23:46 +0300

    Change GlobalLoading styles to module CSS
    Mumo

--- packages/core/src/App/GlobalLoading.jsx
+++ packages/core/src/App/GlobalLoading.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { motion, useAnimation, AnimatePresence } from 'framer-motion';
-import './GlobalLoading.scss';
+import './GlobalLoading.module.scss';
 import LOGO from './Logo/NILOTE.png';
 
 const GlobalLoading = () => {
