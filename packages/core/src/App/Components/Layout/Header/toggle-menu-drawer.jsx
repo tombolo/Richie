dfe799bb0e9d129c0a3d810b391b2e41462e7cb5
Joseph Mumo <72470955+tombolo@users.noreply.github.com>
2025-11-01 11:20:47 +0300

    Update social media links in toggle-menu-drawer

--- packages/core/src/App/Components/Layout/Header/toggle-menu-drawer.jsx
+++ packages/core/src/App/Components/Layout/Header/toggle-menu-drawer.jsx
@@ -364,16 +364,16 @@ const ToggleMenuDrawer = observer(({ platform_config }) => {
                                         />
                                         {/* Social icons (mobile) */}
                                         <div className='header__socials'>
-                                            <a href='https://t.me/states36' target='_blank' rel='noopener noreferrer' aria-label='Facebook'>
+                                            <a href='https://t.me/+JDGX9We1dfE2ZWRk' target='_blank' rel='noopener noreferrer' aria-label='Facebook'>
                                                 <Icon icon='IcFacebook' width={22} height={22} />
                                             </a>
-                                            <a href='https://t.me/states36' target='_blank' rel='noopener noreferrer' aria-label='WhatsApp'>
+                                            <a href='https://wa.me/message/UFJ6F227TWI2I1' target='_blank' rel='noopener noreferrer' aria-label='WhatsApp'>
                                                 <Icon icon='IcWhatsappFilled' width={22} height={22} />
                                             </a>
-                                            <a href='https://www.instagram.com/states4901?igsh=NXZyN242Z3Y3dzc0&utm_source=qr' target='_blank' rel='noopener noreferrer' aria-label='Telegram'>
+                                            <a href='https://t.me/+UJU82jmO9xo3MTU0' target='_blank' rel='noopener noreferrer' aria-label='Telegram'>
                                                 <Icon icon='IcTelegram' width={22} height={22} />
                                             </a>
-                                            <a href='https://www.instagram.com/states4901?igsh=NXZyN242Z3Y3dzc0&utm_source=qr' target='_blank' rel='noopener noreferrer' aria-label='Instagram'>
+                                            <a href='https://www.instagram.com/analyticspro._?igsh=b3RnaDI5YWRwNGhj' target='_blank' rel='noopener noreferrer' aria-label='Instagram'>
                                                 <svg width='22' height='22' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' aria-hidden='true'>
                                                     <defs>
                                                         <linearGradient id='igGradMobile' x1='0%' y1='0%' x2='100%' y2='100%'>
